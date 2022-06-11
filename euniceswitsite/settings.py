d7be27fb68a075415bd73322e74266844b4e5278
Eunice Adebayo <euniceswit@gmail.com>
2022-06-11 18:05:04 +0100

    Simple poll application using Django framework

--- euniceswitsite/settings.py
+++ euniceswitsite/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4i#u*s(mirlwig)jzvu096+wyhvcjfd#g9+n(r^56@9_#_a83q'
+SECRET_KEY = 'XXXX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
